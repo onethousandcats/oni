15ae9c661f94e44696bcf1e06efc34d05d6fbb89
sen <phillip.bercot@gmail.com>
2023-01-09 07:52:44 -0500

    first

---
+++ index.js
@@ -0,0 +1,30 @@
+#! /usr/bin/env node
+
+import chalk from "chalk";
+import boxen from "boxen";
+import path from "path";
+
+import { rags } from "./lib/rags.js";
+
+const usage = "usage: oni <command> [<args>]";
+
+const welcome = boxen(chalk.green("Someone somewhere is wishing for your happiness"),
+        { borderColor: 'green', padding: 1, dimBorder: true  });
+
+const config = rags
+    .setWelcome(welcome)
+    .setUsage(usage)
+    .addCommand("init", init)
+    .addCommand("add", add)
+    .listCommands()
+    .run();
+
+const currentPath = path.basename(process.cwd());
+
+function init(args) {
+    console.log(`init oni directory here`);
+}
+
+function add(args) {
+    console.log(`added files to oni repo`);
+}
